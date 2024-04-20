f77eca18174b2747a82f7854f6f80a9d8f9063b2
ChristianHernand16 <49212986+ChristianHernand16@users.noreply.github.com>
2024-04-20 13:06:11 -0500

    clases

---
+++ Ecommerce/src/main/java/com/mycompany/ecommerce/Clientes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecommerce;
+
+/**
+ *
+ * @author chris
+ */
+public class Clientes {
+    
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String direccion;
+    private String celular;
+    private String email;
+}
---
+++ Ecommerce/src/main/java/com/mycompany/ecommerce/Productos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecommerce;
+
+
+ public class Productos {
+    
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+      
+   }
